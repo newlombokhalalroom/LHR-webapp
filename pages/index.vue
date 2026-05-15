97912dfb2dc69c18443959dd080bb86583578e81
eysar <eysar14@gmail.com>
2026-05-15 17:52:00 +0800

    text change sesele

--- pages/index.vue
+++ pages/index.vue
@@ -179,7 +179,7 @@ onMounted(async () => {
             >
               <div class="p-5 mt-auto space-y-2">
                 <atoms-heading>Destination</atoms-heading>
-                <atoms-text>📌 Located at Sade Village, Pujut District </atoms-text>
+                <atoms-text>📌 Located at Sade Village, Pujut District, Central Lombok </atoms-text>
               </div>
             </div>
           </section>
@@ -197,7 +197,7 @@ onMounted(async () => {
             >
               <div class="p-5 mt-auto space-y-2">
                 <atoms-heading h3>Halal Travel</atoms-heading>
-                <atoms-text>Islamic Center Mosque</atoms-text>
+                <atoms-text>Islamic Center Mosque, Sesele</atoms-text>
               </div>
             </div>
           </section>
