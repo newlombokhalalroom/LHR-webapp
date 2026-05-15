5efacc3ce65ed0a7ddc66863b1911b7db46f99c3
fadilfasa <fadilfasa03@gmail.com>
2026-05-15 15:56:04 +0800

    update button signin GHA

--- pages/index.vue
+++ pages/index.vue
@@ -257,8 +257,8 @@ onMounted(async () => {
             <n-button
               type="primary"
               class="w-full md:w-auto"
-              @click.stop="navigateTo({ path: '/authentication' })"
-              >{{ "Join Now" }}</n-button
+              @click.stop="navigateTo({ path: $dataUser ? '/destinations' : '/authentication' })"
+              >{{ $dataUser ? "Explore Destinations" : "Join Now" }}</n-button
             >
           </div>
         </n-card>
