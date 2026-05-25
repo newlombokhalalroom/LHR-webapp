dd6c93c6d41914a0b2f05bc2756abae517cd7774
fadilfasa <fadilfasa03@gmail.com>
2026-05-25 13:40:32 +0800

    syntax error

--- pages/index.vue
+++ pages/index.vue
@@ -521,7 +521,7 @@ onMounted(async () => {
                             >IDR {{ $addSeparator(_item.price || 0) }}/{{
                               _item.units || "rent"
                             }}</atoms-text
-                          >
+                      
                         </n-tag>
                       </div>
                     </div>
@@ -540,7 +540,7 @@ onMounted(async () => {
         <br />
         <n-button @click="navigateTo({ path: '/hotel' })">See more</n-button>
       </section>
-      
+      <br />
 
       <!-- other -->
       <section class="p-5 border-2 border-opacity-50 border-dashed rounded-md border-primary">
